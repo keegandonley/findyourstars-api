8fc2954152a1e8f6309997bc37e5588702868634
keegandonley <keegan.donley@gmail.com>
2018-11-18 01:51:15 -0800

    proxy ISS api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const Datastore = require('@google-cloud/datastore');
+const Axios = require('axios');
 const projectId = 'sachacks-222818';
 
 exports.http = (request, response) => {
@@ -17,6 +18,22 @@ exports.http = (request, response) => {
   }).catch((e) => {
     response.status(400).send(e ? e : 'Unknown error');
   })
+};
+
+exports.iss = (request, response) => {
+  // CORS
+  response.set('Access-Control-Allow-Origin', "*");
+  response.set('Access-Control-Allow-Methods', 'GET');
+
+  // Retrieve Data
+  Axios.get('http://api.open-notify.org/iss-now.json')
+    .then(function (resp) {
+      response.status(200).send(resp.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      response.status(200).send(error);
+    });
 };
 
 exports.event = (event, callback) => {
