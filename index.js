78ae9f0de02d6781ab5fae501100a0a397bcad0e
keegandonley <keegan.donley@gmail.com>
2018-11-17 20:01:45 -0800

    fetch eclipse data

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
+const Datastore = require('@google-cloud/datastore');
+const projectId = 'sachacks-222818';
 
 exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
+  // CORS
+  response.set('Access-Control-Allow-Origin', "*");
+  response.set('Access-Control-Allow-Methods', 'GET');
+
+  // Retrieve Data
+  const datastore = new Datastore({
+    projectId: projectId,
+  });
+  const query = datastore.createQuery('Eclipse');
+  datastore.runQuery(query).then((result) => {
+    response.status(200).send(result ? result : null);
+  }).catch((e) => {
+    response.status(400).send(e ? e : 'Unknown error');
+  })
 };
 
 exports.event = (event, callback) => {
